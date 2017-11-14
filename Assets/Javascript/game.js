9f043a20ace32760c2ff66186ad60610e8eb4a3a
Patrick Mexal <patrickmexal@gmail.com>
2017-11-14 15:32:44 -0700

    update three

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,12 @@
+
+
+var initialTarget = Math.floor(Math.random() * 100 + 20);
+var gemOne = Math.floor(Math.random() * 12 + 1);
+console.log(gemOne);
+
+document.getElementById("randomnumber").innerHTML = initialTarget;
+
+
+
+
+
