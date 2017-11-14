5caa63fab00f342e6504e7732b99e19af257d9c3
Patrick Mexal <patrickmexal@gmail.com>
2017-11-14 16:05:29 -0700

    update four

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -2,9 +2,15 @@
 
 var initialTarget = Math.floor(Math.random() * 100 + 20);
 var gemOne = Math.floor(Math.random() * 12 + 1);
-console.log(gemOne);
+var gemTwo = Math.floor(Math.random() * 12 + 1);
+var gemThree = Math.floor(Math.random() * 12 + 1);
+var gemFour = Math.floor(Math.random() * 12 + 1);
+var initialScore = 0;
+var totalScore;
 
 document.getElementById("randomnumber").innerHTML = initialTarget;
+
+//totalScore = initialScore + 
 
 
 
