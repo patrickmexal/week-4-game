e3f6cdfc985c843102a23c461cf640b77a4353e1
Patrick Mexal <patrickmexal@gmail.com>
2018-05-07 21:33:02 -0700

    fixed the random number generator

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,10 +1,14 @@
  // Global Variables //
 
 var initialTarget = Math.floor(Math.random() * 101)+ 19;
-var gemOne = Math.floor(Math.random() * 12 + 1);
-var gemTwo = Math.floor(Math.random() * 12 + 1);
-var gemThree = Math.floor(Math.random() * 12 + 1);
-var gemFour = Math.floor(Math.random() * 12 + 1);
+var gemOne;
+var gemTwo;
+var gemThree;
+var gemFour;
+// var gemOne = Math.floor(Math.random() * 12 + 1);
+// var gemTwo = Math.floor(Math.random() * 12 + 1);
+// var gemThree = Math.floor(Math.random() * 12 + 1);
+// var gemFour = Math.floor(Math.random() * 12 + 1);
 var initialScore = 0;
 var totalScore = 0;
 var totalWins = 0;
@@ -14,6 +18,13 @@ var totalLosses = 0;
 function randomNumber() {
 initialTarget = Math.floor(Math.random() * 101) + 19;
 $("#randomnumber").html(initialTarget);
+}
+
+function randomGem() {
+ gemOne = Math.floor(Math.random() * 12 + 1);
+ gemTwo = Math.floor(Math.random() * 12 + 1);
+ gemThree = Math.floor(Math.random() * 12 + 1);
+ gemFour = Math.floor(Math.random() * 12 + 1);
 }
 
 // Sets initial random number target
@@ -76,17 +87,20 @@ function gameOperation() {
 		totalWins++;
 		totalScore = 0;
 		randomNumber();
+		randomGem();
 		$("#wintotal").html("Wins:  " + "" + totalWins);
 	} else if (totalScore > initialTarget) {
 		totalLosses++;
 		totalScore = 0;
 		randomNumber();
+		randomGem();
 		$("#losstotal").html("Losses:  " + "" + totalLosses);
 		}
 }
 
 
 randomNumber();
+randomGem();
 
 console.log(initialTarget);
 
